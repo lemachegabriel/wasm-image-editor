d0415f6736ac395264993707c6d8bc77e4e2beda
lemachegabriel <lemache35@gmail.com>
2024-07-15 22:11:44 -0300

    dealing with memory allocation

--- editor.js
+++ editor.js
@@ -2,9 +2,25 @@ const file = './target/wasm32-unknown-unknown/release/editor.wasm';
 
 WebAssembly.instantiateStreaming(fetch(file))
   .then((wasm) => {
-    const {instance} = wasm
+    const { instance } = wasm
 
-    const {subtracao} = instance.exports
-    console.log(subtracao(29,10))
+    const { subtracao, criar_memoria_inicial, memory, malloc, acumular } = instance.exports
+
+    criar_memoria_inicial()
+
+    const jsList = Uint8Array.from([20, 50, 80])
+    const jsListLenght = jsList.length
+    const wasmListPointer = malloc(jsListLenght)
+    const wasmList = new Uint8Array(memory.buffer, wasmListPointer, jsListLenght)
+
+    wasmList.set(jsList)
+    const sumBetweenListItens = acumular(wasmListPointer, jsListLenght)
+
+    criar_memoria_inicial()
+    const arrayMemoria = new Uint8Array(memory.buffer, 0).slice(0, 10);
+    console.log(arrayMemoria)
+
+
+    console.log(sumBetweenListItens)
   });
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,47 @@
+use core::slice::from_raw_parts_mut;
+use std::mem;
+use std::alloc::{alloc, Layout};
+
+#[no_mangle]
+pub fn criar_memoria_inicial() {
+    let fatia: &mut [u8];
+
+    unsafe {
+        fatia = from_raw_parts_mut::<u8>(6 as *mut u8, 10);
+    }
+
+    fatia[0] = 85;
+}
+
+#[no_mangle]
+extern fn malloc(comprimento: usize) -> *mut u8 {
+    let alinhamento = mem::align_of::<usize>();
+    if let Ok(layout) = Layout::from_size_align(comprimento, alinhamento) {
+        unsafe {
+            if layout.size() > 0 {
+                let ponteiro = alloc(layout);
+                if !ponteiro.is_null() {
+                    return ponteiro;
+                }
+            } else {
+                return alinhamento as *mut u8;
+            }
+        }
+    }
+    std::process::abort()
+}
+
+#[no_mangle]
+extern fn acumular(ponteiro: *mut u8, comprimento: usize) -> i32 {
+    let fatia = unsafe { from_raw_parts_mut(ponteiro as *mut u8, comprimento) };
+    let mut soma = 0;
+    for i in 0..comprimento {
+        soma = soma + fatia[i];
+    }
+    soma as i32
+}
+
+
 #[no_mangle]
 pub fn subtracao(numero_a: u8, numero_b: u8) -> u8 {
     numero_a - numero_b
